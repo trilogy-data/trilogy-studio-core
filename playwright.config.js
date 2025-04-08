d053692c53c7c4d3f2a5ce92adf0d558d5ed1d21
efromvt <ethan.dickinson@gmail.com.com>
2025-04-07 22:25:26 -0400

    fix CI retries

--- playwright.config.js
+++ playwright.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
   },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 2 : undefined,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:5173',
