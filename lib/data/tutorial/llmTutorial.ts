944095699deb26d7c3a5d3bd9ad3d28f83d5512a
efromvt <ethan.dickinson@gmail.com.com>
2025-04-27 12:09:08 -0400

    more tweaks

--- lib/data/tutorial/llmTutorial.ts
+++ lib/data/tutorial/llmTutorial.ts
@@ -16,7 +16,12 @@ export const llmTutorial = new Article('LLM Connections', [
       ),
       new Paragraph(
         'Managing Connections',
-        'You can view current LLM connections below. Only one LLM connection can be the default at a time.',
+        'You can view current LLM connections below. Only one LLM connection can be the default at a time. You can set a default by clicking the star icon. The default LLM will be used for all LLM functionality. Try setting one up, confirm you can connect, select an appropriate model, and save. Then head to an editor or a dashboard to try it out!',
       ),
       new Paragraph('LLMList', '', 'llm-connections'),
-    ]),
+      new Paragraph(
+        'Connections',
+        'LLMs operate on the imported concept context. Make sure you have a datasource selected on a dashboard, or add imports to an editor before typing your comment. Ctrl-shift-enter is the default LLM activation shortcut; typically you will highlight text and then run this.',
+        'tip'
+      ),
+    ])
