4c7a44e46746ddadfd95d2a05428a0047c9463b7
efromvt <ethan.dickinson@gmail.com.com>
2025-07-01 07:42:01 -0400

    config cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vitest/config'
 
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   test: {
-    environment: 'jsdom', // or 'happy-dom'
-    // other test config
-
+    environment: 'jsdom',
     exclude: [
       'e2e/**', // Exclude Playwright tests
       '**/node_modules/**',
