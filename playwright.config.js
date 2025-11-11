718ae5cfd5851b809676c0b0f9fa184ef7cbf21b
efromvt <ethan.dickinson@gmail.com.com>
2025-11-11 09:19:30 -0500

    skip webserver entirely in docker

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@
 import { defineConfig, devices } from '@playwright/test'
 
 const usePreview = process.env.PLAYWRIGHT_USE_PREVIEW === 'true'
-const inDocker = process.env.TEST_ENV ==='docker'
+const inDocker = process.env.TEST_ENV === 'docker'
 export default defineConfig({
   testDir: './e2e',
   timeout: 120000,
@@ -15,7 +15,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL:  inDocker ? 'http://localhost:8080' : 'http://localhost:5173',
+    baseURL: inDocker ? 'http://localhost:8080' : 'http://localhost:5173',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
@@ -41,12 +41,9 @@ export default defineConfig({
       use: { ...devices['iPhone 13'] },
     },
   ],
-  webServer: {
-    // if in docker; skip
+  webServer: inDocker ? undefined : {
     command: usePreview ? 'pnpm preview --port 5173' : 'pnpm dev',
-    // command: 'pnpm preview --port 5173',
-    port:  inDocker? 8080: 5173,
-    // in docker tests, we are running the image, use that as abase
-    reuseExistingServer: inDocker || !process.env.CI,
+    port: 5173,
+    reuseExistingServer: !process.env.CI,
   },
 })
