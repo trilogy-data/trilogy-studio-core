b9c85b801fcd91608db895fdb59f0e7605ee4bec
efromvt <ethan.dickinson@gmail.com.com>
2026-03-01 13:18:58 -0500

    more fixes

--- lib/components/sidebar/Sidebar.vue
+++ lib/components/sidebar/Sidebar.vue
@@ -122,8 +122,8 @@
 
     <div
       class="sidebar-content"
-      :class="{ 'sidebar-content-collapsed': contentCollapsed && !isMobile() }"
-      :style="contentCollapsed && !isMobile() ? {} : { width: containerWidth - 40 + 'px' }"
+      :class="{ 'sidebar-content-collapsed': contentCollapsed && !isMobile }"
+      :style="contentCollapsed && !isMobile ? {} : { width: containerWidth - 40 + 'px' }"
     >
       <EditorList
         :activeEditor="activeEditor"
@@ -355,7 +355,7 @@ export default defineComponent({
       }, 500)
     },
     selectItem(index: string) {
-      if (this.isMobile()) {
+      if (this.isMobile) {
         this.$emit('screen-selected', index)
         return
       }
